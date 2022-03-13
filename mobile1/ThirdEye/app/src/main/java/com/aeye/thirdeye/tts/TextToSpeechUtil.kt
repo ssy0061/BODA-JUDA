17e6186cbae5a95a081507abc594009cb02e3bd3
2dend <2dend0713@gmail.com>
2022-03-13 17:51:26 +0900

    S06P21S004-188 [mo] feat: ttsCustom에 pitch와 speed 조절 가능하도록 변경

--- mobile1/ThirdEye/app/src/main/java/com/aeye/thirdeye/tts/TextToSpeechUtil.kt
+++ mobile1/ThirdEye/app/src/main/java/com/aeye/thirdeye/tts/TextToSpeechUtil.kt
@@ -1,14 +1,14 @@
 package com.aeye.thirdeye.vibrator
 
 import android.app.Activity
-import android.content.Context
-import android.os.Build
 import android.speech.tts.TextToSpeech
 import android.util.Log
-import androidx.annotation.RequiresApi
 import java.util.*
 
-class TextToSpeechUtil(val activity: Activity, val txt: String): TextToSpeech.OnInitListener {
+class TextToSpeechUtil(val activity: Activity,
+                       val txt: String,
+                       val pit: Double = 1.0,
+                       val spd: Double = 1.0): TextToSpeech.OnInitListener {
 
     val ttsCustom = TextToSpeech(activity, this)
 
@@ -19,6 +19,8 @@ class TextToSpeechUtil(val activity: Activity, val txt: String): TextToSpeech.On
             if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
                 Log.d("TTS", "unsupported language")
             } else {
+                ttsCustom.setPitch(pit.toFloat())
+                ttsCustom.setSpeechRate(spd.toFloat())
                 speakTxt(txt)
             }
         } else {
