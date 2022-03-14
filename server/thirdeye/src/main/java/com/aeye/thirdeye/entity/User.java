edbf2a82bd1d0e0a4871afa9199f22c32e9a81e8
임다훈 <dahontop@gmail.com>
2022-03-14 16:29:39 +0900

    S06P21S004-260 [be] feat: User Entity 기본 생성

--- server/thirdeye/src/main/java/com/aeye/thirdeye/entity/User.java
+++ server/thirdeye/src/main/java/com/aeye/thirdeye/entity/User.java
@@ -1,2 +1,35 @@
-package com.aeye.thirdeye.entity;public class User {
+package com.aeye.thirdeye.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "User")
+@Getter
+@Setter
+@NoArgsConstructor//(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Size(max = 30)
+    private String userId;
+
+    @Size(max = 20)
+    private String password;
+
+    private String email;
+
+    @Size(max = 20)
+    private String nickName;
+
 }
