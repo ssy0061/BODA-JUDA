31142ffcf22e688fe057687eed270cc566a55405
2dend <2dend0713@gmail.com>
2022-03-17 08:58:08 +0900

    [mo] feat: (app2) MainActivity 수정

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/MainActivity.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/MainActivity.kt
@@ -2,6 +2,8 @@ package com.aeye.nextlabel.feature.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.aeye.nextlabel.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,12 +11,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setHomeFragment()
+        settingHomeFragment()
     }
 
-    fun setHomeFragment() {
+    override fun onResume() {
+        super.onResume()
+        settingButton()
+        settingNavBar()
+    }
+
+    fun settingHomeFragment() {
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.area_fragment, HomeFragment())
         fragmentTransaction.commit()
+    }
+
+    fun settingButton() {
+        val labelingBtn = findViewById<Button>(R.id.btn_labeling)
+
+        labelingBtn?.setOnClickListener {
+            val fragmentTransaction = supportFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.area_fragment, CameraFragment())
+            fragmentTransaction.commit()
+        }
+    }
+
+    fun settingNavBar() {
+        val homeTxt = findViewById<TextView>(R.id.nav_home)
+        val labelingTxt = findViewById<TextView>(R.id.nav_labeling)
+
+        homeTxt?.setOnClickListener {
+            val fragmentTransaction = supportFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.area_fragment, HomeFragment())
+            fragmentTransaction.commit()
+        }
+        labelingTxt?.setOnClickListener {
+            val fragmentTransaction = supportFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.area_fragment, CameraFragment())
+            fragmentTransaction.commit()
+        }
     }
 }
