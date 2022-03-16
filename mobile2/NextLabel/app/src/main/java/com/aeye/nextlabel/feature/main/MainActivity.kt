3f9144845683670d366657c7f4107353666eb637
김민수 <ms001118@naver.com>
2022-03-16 10:22:01 +0900

    S06P21S004-286 [mo] fix: MainActivity 이동

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/MainActivity.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.aeye.nextlabel
+package com.aeye.nextlabel.feature.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aeye.nextlabel.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
