ea0f7048656666c97a5bba305735214636023385
2dend <2dend0713@gmail.com>
2022-03-17 18:06:21 +0900

    [mo] feat: (app2) Preview 기능 구현

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/CameraFragment.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/CameraFragment.kt
@@ -1,18 +1,53 @@
 package com.aeye.nextlabel.feature.main
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.aeye.nextlabel.R
+import com.aeye.nextlabel.databinding.FragmentCameraBinding
+import com.google.common.util.concurrent.ListenableFuture
 
 class CameraFragment: Fragment() {
+
+    val binding by lazy { FragmentCameraBinding.inflate(layoutInflater) }
+    lateinit var activity: MainActivity
+    lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_camera, container, false)
+        activity = context as MainActivity
+
+        startCamera()
+
+        return binding.root
+    }
+
+    fun startCamera() {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(activity)
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            val preview = getPreview()
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+        }, ContextCompat.getMainExecutor(activity))
+    }
+
+    fun getPreview(): Preview {
+        val preview: Preview = Preview.Builder().build()
+        preview.setSurfaceProvider(binding.pvCamera.getSurfaceProvider())
+
+        return preview
     }
 }
