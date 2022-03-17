bc67fedd58aa4e0299ade9413ea1ed8edfebfd5a
2dend <2dend0713@gmail.com>
2022-03-17 23:17:08 +0900

    S06P21S004-239 [mo] feat: (app2)  디렉토리 생성 코드 작성

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/CameraFragment.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/CameraFragment.kt
@@ -7,23 +7,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.ActivityChooserView
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.aeye.nextlabel.R
 import com.aeye.nextlabel.databinding.FragmentCameraBinding
 import com.google.common.util.concurrent.ListenableFuture
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class CameraFragment: Fragment() {
-
-    val PERMISSIONS_REQUEST_CODE = 1
-    val PERMISSIONS_REQUIRED = arrayOf(Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE)
 
     val binding by lazy { FragmentCameraBinding.inflate(layoutInflater) }
     lateinit var activity: MainActivity
     lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+
+    private var imageCapture: ImageCapture? = null
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,11 +40,15 @@ class CameraFragment: Fragment() {
     ): View? {
         activity = context as MainActivity
 
-        if (!hasPermissions(activity)) {
-            requestPermissions(PERMISSIONS_REQUIRED, PERMISSIONS_REQUEST_CODE)
-        } else {
+        if (hasPermissions(activity)) {
             startCamera()
+        } else {
+            ActivityCompat.requestPermissions(activity, PERMISSIONS_REQUIRED, PERMISSIONS_REQUEST_CODE)
         }
+
+        binding.btnCamera.setOnClickListener { takePhoto() }
+        outputDirectory = getOutputDirectory()
+        cameraExecutor = Executors.newSingleThreadExecutor()
 
         return binding.root
     }
@@ -55,6 +67,7 @@ class CameraFragment: Fragment() {
         if (requestCode == PERMISSIONS_REQUEST_CODE) {
             if (PackageManager.PERMISSION_GRANTED == grantResults.firstOrNull()) {
                 startCamera()
+
             } else {
                 activity.finish()
             }
@@ -68,8 +81,9 @@ class CameraFragment: Fragment() {
 
             val preview = getPreview()
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            imageCapture = ImageCapture.Builder().build()
 
-            cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
         }, ContextCompat.getMainExecutor(activity))
     }
 
@@ -78,5 +92,28 @@ class CameraFragment: Fragment() {
         preview.setSurfaceProvider(binding.pvCamera.getSurfaceProvider())
 
         return preview
+    }
+
+    fun takePhoto() {
+
+    }
+
+    fun getOutputDirectory(): File {
+        val mediaDir = activity.externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+        return if (mediaDir != null && mediaDir.exists()) mediaDir else activity.filesDir
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        val PERMISSIONS_REQUEST_CODE = 10
+        val PERMISSIONS_REQUIRED = arrayOf(Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE)
     }
 }
