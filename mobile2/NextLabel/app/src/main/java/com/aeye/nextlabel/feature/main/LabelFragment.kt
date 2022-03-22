823a58f63878a433b9eb433f88e4c01eb6aa7991
김민수 <ms001118@naver.com>
2022-03-22 14:07:11 +0900

    S06P21S004-350 [mo] feat: viewModel에서 imageUri 가져와 세팅

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/LabelFragment.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/LabelFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.aeye.nextlabel.R
 import com.aeye.nextlabel.databinding.FragmentLabelBinding
 import com.aeye.nextlabel.feature.common.BaseFragment
@@ -12,9 +13,17 @@ import com.aeye.nextlabel.feature.common.BaseFragment
 class LabelFragment : BaseFragment<FragmentLabelBinding>(FragmentLabelBinding::bind, R.layout.fragment_label) {
     private val TAG = "LabelFragment_debuk"
 
+    private val viewModel: ContainerViewModel by viewModels(ownerProducer = {requireParentFragment()})
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        init()
     }
 
+    private fun init() {
+        viewModel.imageSavedUri?.let{
+            binding.imageViewLabelF.setImageURI(it)
+        }
+    }
 }
