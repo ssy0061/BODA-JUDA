b74cb44a79244403a36c8468db78ef5fb2a41b21
김유진 <determination9212@gmail.com>
2022-03-22 19:35:07 +0900

    S06P21S004-361 [be] feat: firebase remote-config 변수 값 읽기 및 수정 성공

--- trainingServer/AEye02/controller.py
+++ trainingServer/AEye02/controller.py
@@ -1,11 +1,13 @@
 from image_maker import image_maker as im
 from snack_classifier import snack_model_maker as smm
 
-origin_image_path = '/home/team1/AEye/data/image/snack_4'
-input_image_path = origin_image_path + '_rota'
-model_name = 'snack_v8'
-model_path = '/home/team1/AEye/model_float16'
-key_path = '/home/team1/AEye/keys'
-key_name = 'aeye-9c1ee-firebase-adminsdk-9glw7-8f233a41fb.json'
-im(origin_image_path)
-smm(model_name,input_image_path,model_path,key_path, key_name)
+ORIGIN_IMAGE_PATH = '/home/team1/AEye/data/image/snack_4'
+INPUT_IMAGE_PATH = ORIGIN_IMAGE_PATH + '_rota'
+MODEL_NAME = 'snack_v8'
+MODEL_PATH = '/home/team1/AEye/model_float16'
+KEY_PATH = '/home/team1/AEye/keys'
+KEY_NAME = 'aeye-9c1ee-firebase-adminsdk-9glw7-8f233a41fb.json'
+
+
+im(ORIGIN_IMAGE_PATH)
+smm(MODEL_NAME,INPUT_IMAGE_PATH,MODEL_PATH,KEY_PATH, KEY_NAME)
