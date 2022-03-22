5ddb0114eb9273f93de29add61fa85ce361b73b4
2dend <2dend0713@gmail.com>
2022-03-22 16:17:03 +0900

    [mo] feat: (app2) api 통신 연습을 위한 임시 dto 작성

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/temp/api/UserApi.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/temp/api/UserApi.kt
@@ -1,25 +1,28 @@
 package com.aeye.nextlabel.temp.api
 
+import com.aeye.nextlabel.temp.dto.UserJoin
+import com.aeye.nextlabel.temp.dto.UserLeave
+import com.aeye.nextlabel.temp.dto.UserLogin
 import retrofit2.Response
 import retrofit2.http.*
 
 interface UserApi {
-    // 모든 상대 주소는 임시로 작성한 것들임
-    // 차후 API 개발이 완료되면 수정할 예정임
+    // 모든 상대 주소는 임시로 작성한 것들
+    // 차후 API 개발이 완료되면 수정할 예정
 
     // 1. 회원 가입
     @POST("/api/user/signup")
-    suspend fun join(@Body user: UserForJoin): Response<JoinResponse>
+    suspend fun join(@Body user: UserJoin): Response<JoinResponse>
 
     // 2. 로그인
     @POST("/api/user/login")
-    suspend fun login(@Body user: UserForLogin): Response<LoginResponse>
+    suspend fun login(@Body user: UserLogin): Response<LoginResponse>
 
     // 3. 회원 정보 수정
     @PUT("/api/user/update")
-    suspend fun update(@Body user: UserForJoin): Response<JoinResponse>
+    suspend fun update(@Body user: UserJoin): Response<JoinResponse>
 
     // 4. 회원 탈퇴
     @DELETE("/api/user/signout")
-    suspend fun leave(@Body user: UserForLogin): Response<LoginResponse>
+    suspend fun leave(@Body user: UserLeave): Response<LoginResponse>
 }
