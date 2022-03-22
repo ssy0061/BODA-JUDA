d64c982bd2d5b3c1290960e51eb81448a8a79f8d
김민수 <ms001118@naver.com>
2022-03-22 13:07:45 +0900

    S06P21S004-350 [mo] feat: LabelFragment 추가

---
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/LabelFragment.kt
@@ -0,0 +1,20 @@
+package com.aeye.nextlabel.feature.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.aeye.nextlabel.R
+import com.aeye.nextlabel.databinding.FragmentLabelBinding
+import com.aeye.nextlabel.feature.common.BaseFragment
+
+class LabelFragment : BaseFragment<FragmentLabelBinding>(FragmentLabelBinding::bind, R.layout.fragment_label) {
+    private val TAG = "LabelFragment_debuk"
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
