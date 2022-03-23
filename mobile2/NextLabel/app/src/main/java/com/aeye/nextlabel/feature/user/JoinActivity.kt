e5442f2b3224849dbef6d7392d82596dea16842c
2dend <2dend0713@gmail.com>
2022-03-23 11:38:27 +0900

    [mo] feat: (app2) start developing "sign up(join)" activity and related stuff

---
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/user/JoinActivity.kt
@@ -0,0 +1,11 @@
+package com.aeye.nextlabel.feature.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+    }
+}
