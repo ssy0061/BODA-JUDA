11e80d880b50f76ef296397cfe002e8fac333ce4
2dend <2dend0713@gmail.com>
2022-03-24 12:05:20 +0900

    [mo] feat: (app2) sign up and log in UserApi 수정

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/model/network/api/UserApi.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/model/network/api/UserApi.kt
@@ -1,13 +1,10 @@
 package com.aeye.nextlabel.model.network.api
 
-import com.aeye.nextlabel.model.dto.UserJoin
-import com.aeye.nextlabel.model.dto.UserLeave
-import com.aeye.nextlabel.model.dto.UserLogin
 import com.aeye.nextlabel.model.network.response.JoinResponse
-import com.aeye.nextlabel.model.network.response.LeaveResponse
 import com.aeye.nextlabel.model.network.response.LoginResponse
 import retrofit2.Response
-import retrofit2.http.*
+import retrofit2.http.Body
+import retrofit2.http.POST
 
 interface UserApi {
     // 1. Sign Up
@@ -15,14 +12,15 @@ interface UserApi {
     suspend fun join(@Body user: UserForJoin): Response<JoinResponse>
 
     // 2. Sign Out
-    @POST("/accounts/signout")
-    suspend fun emailLogin(@Body user: UserForLogin): Response<LoginResponse>
 
     // 3. Log In (일반)
     @POST("/accounts/login")
-    suspend fun emailLogin(@Body user: UserForLogin): Response<LoginResponse>
+    suspend fun login(@Body user: UserForLogin): Response<LoginResponse>
 
     // 4. Log In (소셜)
 
+    // 5. 회원정보 수정(without password)
+
+    // 6. 비밀번호 수정
 
 }
