9a7cef45dc7f84bd2d51884e168056395e4be4bc
2dend <2dend0713@gmail.com>
2022-03-24 13:25:28 +0900

    [mo] feat: (app2) UserRepository 수정

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/common/BaseResponse.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/common/BaseResponse.kt
@@ -1,5 +1,10 @@
 package com.aeye.nextlabel.feature.common
 
-open class BaseResponse {
+import com.google.gson.annotations.SerializedName
 
+open class BaseResponse {
+    @SerializedName("output")
+    val output: Int = 0
+    @SerializedName("msg")
+    val message: String? = null
 }
--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/repository/UserRepository.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/repository/UserRepository.kt
@@ -4,10 +4,8 @@ import com.aeye.nextlabel.global.ApplicationClass
 import com.aeye.nextlabel.util.Resource
 import com.aeye.nextlabel.model.network.api.UserApi
 import com.aeye.nextlabel.model.dto.UserForJoin
-import com.aeye.nextlabel.model.dto.UserLeave
-import com.aeye.nextlabel.model.dto.UserLogin
+import com.aeye.nextlabel.model.dto.UserForLogin
 import com.aeye.nextlabel.model.network.response.JoinResponse
-import com.aeye.nextlabel.model.network.response.LeaveResponse
 import com.aeye.nextlabel.model.network.response.LoginResponse
 import java.lang.Exception
 
@@ -17,7 +15,7 @@ class UserRepository {
     suspend fun join(user: UserForJoin): Resource<JoinResponse> {
         return try {
             val response = userApi.join(user)
-            if(response.isSuccessful) {
+            if (response.isSuccessful) {
                 return if(response.code() == 200 && response.body()!!.output == 1) {
                     Resource.success(response.body()!!)
                 } else {
@@ -31,44 +29,10 @@ class UserRepository {
         }
     }
 
-    suspend fun login(user: UserLogin): Resource<LoginResponse> {
+    suspend fun login(user: UserForLogin): Resource<LoginResponse> {
         return try {
             val response = userApi.login(user)
-            if(response.isSuccessful) {
-                return if(response.code() == 200 && response.body()!!.output == 1) {
-                    Resource.success(response.body()!!)
-                } else {
-                    Resource.error(null, response.body()!!.message!!)
-                }
-            } else {
-                Resource.error(null, "알 수 없는 오류입니다.")
-            }
-        } catch (e: Exception) {
-            Resource.error(null, "서버와 연결할 수 없습니다. 잠시 후 다시 시도해 주세요.")
-        }
-    }
-
-    suspend fun update(user: UserForJoin): Resource<JoinResponse> {
-        return try {
-            val response = userApi.update(user)
-            if(response.isSuccessful) {
-                return if(response.code() == 200 && response.body()!!.output == 1) {
-                    Resource.success(response.body()!!)
-                } else {
-                    Resource.error(null, response.body()!!.message!!)
-                }
-            } else {
-                Resource.error(null, "알 수 없는 오류입니다.")
-            }
-        } catch (e: Exception) {
-            Resource.error(null, "서버와 연결할 수 없습니다. 잠시 후 다시 시도해 주세요.")
-        }
-    }
-
-    suspend fun leave(user: UserLeave): Resource<LeaveResponse> {
-        return try {
-            val response = userApi.leave(user)
-            if(response.isSuccessful) {
+            if (response.isSuccessful) {
                 return if(response.code() == 200 && response.body()!!.output == 1) {
                     Resource.success(response.body()!!)
                 } else {
