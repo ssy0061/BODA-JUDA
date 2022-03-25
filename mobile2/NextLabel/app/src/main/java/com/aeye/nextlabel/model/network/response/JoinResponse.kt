8132138f680ff8310258b3a97503df48f0a6caa2
2dend <2dend0713@gmail.com>
2022-03-25 14:09:25 +0900

    [mo] feat: (app2) sign up 부분 조정

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/model/network/response/JoinResponse.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/model/network/response/JoinResponse.kt
@@ -1,7 +1,15 @@
 package com.aeye.nextlabel.model.network.response
 
 import com.aeye.nextlabel.feature.common.BaseResponse
+import com.google.gson.annotations.SerializedName
 
 class JoinResponse: BaseResponse() {
+    @SerializedName("body")
+    val dataSet: String? = null
 
+    @SerializedName("statusCode")
+    val statusCode: String? = null
+
+    @SerializedName("statusCodeValue")
+    val statusCodeValue: Int? = null
 }
