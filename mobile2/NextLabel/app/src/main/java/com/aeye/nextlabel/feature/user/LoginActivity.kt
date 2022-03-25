5ce991d5a9223e030ac86e4c57b7b57d77e3e853
2dend <2dend0713@gmail.com>
2022-03-25 14:21:11 +0900

    temp

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/user/LoginActivity.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/user/LoginActivity.kt
@@ -2,11 +2,15 @@ package com.aeye.nextlabel.feature.user
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.aeye.nextlabel.databinding.ActivityLoginBinding
 import com.aeye.nextlabel.feature.common.BaseActivity
 import com.aeye.nextlabel.feature.main.MainActivity
+import com.aeye.nextlabel.model.dto.UserForLogin
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate) {
+
+    val userViewModel: UserViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,5 +30,12 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::i
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun login() {
+        val userId = binding.userId.text.toString()
+        val password = binding.password.text.toString()
+
+        userViewModel.login(UserForLogin(userId, password))
     }
 }
