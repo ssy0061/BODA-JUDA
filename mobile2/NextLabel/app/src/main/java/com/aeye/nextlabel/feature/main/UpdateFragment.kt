ae6055bfdd981eeb29ac92ec4060183f7d775b09
sangyong <dbr00061@naver.com>
2022-03-25 19:24:13 +0900

    S06P21S004-365 [mo] 회원정보 수정 레이아웃

---
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/UpdateFragment.kt
@@ -0,0 +1,48 @@
+package com.aeye.nextlabel.feature.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.aeye.nextlabel.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [UpdateFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class UpdateFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_update, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment UpdateFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            UpdateFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}
