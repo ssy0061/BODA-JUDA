ee08b5c6ec79ea69a499afad6afaca302e7170de
김민수 <ms001118@naver.com>
2022-03-28 17:45:48 +0900

    S06P21S004-354 [mo] feat: Bounding Box 좌표 컨버터 추가

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/util/BoundingBoxConverter.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/util/BoundingBoxConverter.kt
@@ -1,4 +1,13 @@
 package com.aeye.nextlabel.util
 
+import kotlin.math.floor
+
 object BoundingBoxConverter {
+    fun convertToImageCoor(imageWidth: Int, imageHeight: Int, overlayViewWidth: Int, overlayViewHeight: Int, ltrb: Array<Float>): Array<Int> {
+        val left = floor(ltrb[0].toDouble() / overlayViewWidth * imageWidth).toInt()
+        val top = floor(ltrb[1].toDouble() / overlayViewHeight * imageHeight).toInt()
+        val right = floor(ltrb[2].toDouble() / overlayViewWidth * imageWidth).toInt()
+        val bottom = floor(ltrb[3].toDouble() / overlayViewHeight * imageHeight).toInt()
+        return arrayOf(left, top, right, bottom)
+    }
 }
