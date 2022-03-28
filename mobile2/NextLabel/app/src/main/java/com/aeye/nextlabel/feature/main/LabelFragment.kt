f71871db3b6518f68f68158784e9903172b3c3c1
김민수 <ms001118@naver.com>
2022-03-28 17:46:20 +0900

    S06P21S004-354 [mo] feat: 제출 버튼 클릭 리스너 추가

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/LabelFragment.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/LabelFragment.kt
@@ -1,6 +1,7 @@
 package com.aeye.nextlabel.feature.main
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import androidx.fragment.app.viewModels
 import com.aeye.nextlabel.R
 import com.aeye.nextlabel.databinding.FragmentLabelBinding
 import com.aeye.nextlabel.feature.common.BaseFragment
+import com.aeye.nextlabel.util.BoundingBoxConverter
 
 class LabelFragment : BaseFragment<FragmentLabelBinding>(FragmentLabelBinding::bind, R.layout.fragment_label) {
     private val TAG = "LabelFragment_debuk"
@@ -26,13 +28,21 @@ class LabelFragment : BaseFragment<FragmentLabelBinding>(FragmentLabelBinding::b
         }
 
         binding.buttonLabelFSubmit.setOnClickListener {
-            val ltrb = setBoundingBoxCoor()
-            binding.editTextLabelFProductName.text.toString()
-            binding.editTextLabelFManufacturer.text.toString()
+            val boundingBoxCoor = setBoundingBox()
+            Log.d(TAG, "init: ${boundingBoxCoor}")
+            val name = binding.editTextLabelFProductName.text.toString()
+            val manufacturer = binding.editTextLabelFManufacturer.text.toString()
         }
     }
 
-    private fun setBoundingBox(): Array<Float> {
-        binding.boxImageViewLabelF.
+    private fun setBoundingBox(): Array<Int> {
+        val ltrb = binding.boxImageViewLabelF.getRectCoor()
+        val imageWidth = binding.boxImageViewLabelF.imageWidth
+        val imageHeight = binding.boxImageViewLabelF.imageLength
+        val viewLTRB = binding.boxImageViewLabelF.limitTRB
+        if (imageWidth != null && imageHeight != null) {
+            return BoundingBoxConverter.convertToImageCoor(imageWidth, imageHeight, viewLTRB[2] - viewLTRB[0], viewLTRB[3] - viewLTRB[1], ltrb)
+        }
+        return emptyArray()
     }
 }
