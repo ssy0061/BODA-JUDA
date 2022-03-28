175915080edc0ae3ebe29d34e67a3a56b0be257a
2dend <2dend0713@gmail.com>
2022-03-28 13:33:03 +0900

    S06P21S004-308 [mo] feat: (app2) Sign Up 기능 동작 확인(201 코드 반환)

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/repository/LabelingRepository.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/repository/LabelingRepository.kt
@@ -9,20 +9,20 @@ import com.aeye.nextlabel.util.Resource
 class LabelingRepository {
     var labelingApi: LabelingApi = ApplicationClass.sRetrofit.create(LabelingApi::class.java)
 
-    suspend fun giveLabels(label: Label): Resource<LabelingResponse> {
-        return try {
-            val response = labelingApi.giveLabels(label)
-            if (response.isSuccessful) {
-                return if(response.code() == 200 && response.body()!!.output == 1) {
-                    Resource.success(response.body()!!)
-                } else {
-                    Resource.error(null, response.body()!!.message!!)
-                }
-            } else {
-                Resource.error(null, "알 수 없는 오류입니다.")
-            }
-        } catch (e: Exception) {
-            Resource.error(null, "서버와 연결할 수 없습니다. 잠시 후 다시 시도해 주세요.")
-        }
-    }
+//    suspend fun giveLabels(label: Label): Resource<LabelingResponse> {
+//        return try {
+//            val response = labelingApi.giveLabels(label)
+//            if (response.isSuccessful) {
+//                return if(response.code() == 200 && response.body()!!.output == 1) {
+//                    Resource.success(response.body()!!)
+//                } else {
+//                    Resource.error(null, response.body()!!.message!!)
+//                }
+//            } else {
+//                Resource.error(null, "알 수 없는 오류입니다.")
+//            }
+//        } catch (e: Exception) {
+//            Resource.error(null, "서버와 연결할 수 없습니다. 잠시 후 다시 시도해 주세요.")
+//        }
+//    }
 }
