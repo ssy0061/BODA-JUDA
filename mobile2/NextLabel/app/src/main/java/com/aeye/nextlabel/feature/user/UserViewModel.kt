e27e1526b0c79b33c0274f3795a7897e1f521dc6
2dend <2dend0713@gmail.com>
2022-03-28 20:42:23 +0900

    S06P21S004-309 [mo] feat: (app2) password update api 수정2

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/user/UserViewModel.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/user/UserViewModel.kt
@@ -1,6 +1,5 @@
 package com.aeye.nextlabel.feature.user
 
-import android.net.Uri
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
