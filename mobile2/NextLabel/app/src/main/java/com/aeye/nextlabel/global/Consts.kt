cc568e4feed948166a01c16235332cef2db82954
김민수 <ms001118@naver.com>
2022-03-29 17:35:17 +0900

    S06P21S004-415 [mo] feat: Fragment이동을 위한 키 추가

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/global/Consts.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/global/Consts.kt
@@ -4,3 +4,8 @@ package com.aeye.nextlabel.global
 const val REQUEST_KEY_TO_MOVE = "moveNext"
 const val BUNDLE_KEY_TO_MOVE = "moveNextBundle"
 // const val SUCCESS = 200
+
+// fragment 이동
+const val JOIN_FRAGMENT = "JoinFragment"
+const val LOGIN_FRAGMENT = "LoginFragment"
+const val FRAGMENT_BUNDLE_KEY = "FragmentKey"
