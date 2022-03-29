af3f03adeee94236a78955103cc5702deb5ffd05
김민수 <ms001118@naver.com>
2022-03-29 17:11:12 +0900

    S06P21S004-415 [mo] feat: JoinFragment 생성

---
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/user/JoinFragment.kt
@@ -0,0 +1,105 @@
+package com.aeye.nextlabel.feature.user
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.aeye.nextlabel.R
+import com.aeye.nextlabel.databinding.FragmentJoinBinding
+import com.aeye.nextlabel.feature.common.BaseFragment
+import com.aeye.nextlabel.feature.main.MainActivity
+import com.aeye.nextlabel.model.dto.UserForJoin
+import com.aeye.nextlabel.util.InputValidUtil
+import com.aeye.nextlabel.util.Status
+
+class JoinFragment : BaseFragment<FragmentJoinBinding>(FragmentJoinBinding::bind, R.layout.fragment_join) {
+    private val userViewModel: UserViewModel by activityViewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+        initLiveDataObserver()
+    }
+
+    private fun init() {
+        val btnLogin = binding.textButtonLogin  // button login
+        val btnJoin = binding.containedButtonJoin  // button join: move to home
+
+        btnLogin.setOnClickListener {
+            // TODO: LoginFragment로 이동
+        }
+
+        btnJoin.setOnClickListener {
+            if (checkInputForm()) {
+                join()
+            }
+        }
+    }
+
+    private fun initLiveDataObserver() {
+        userViewModel.joinRequestLiveData.observe(requireActivity()) {
+            when(it.status) {
+                Status.LOADING -> {
+                    // TODO: showLoading()
+                }
+                Status.SUCCESS -> {
+                    // TODO: dismissLoading()
+                    val intent = Intent(requireActivity(), MainActivity::class.java)
+                    startActivity(intent)
+                    requireActivity().finish()
+                }
+                Status.ERROR -> {
+                    // TODO: dismissLoading()
+                }
+            }
+        }
+    }
+
+    private fun join() {
+        val userId = binding.userId.text.toString()
+        val password = binding.password.text.toString()
+        val email = binding.email.text.toString()
+        val nickname = binding.nickname.text.toString()
+
+        userViewModel.join(UserForJoin(userId, password, email, nickname))
+    }
+
+    private fun checkInputForm(): Boolean {
+        var result = 1
+
+        val userId = binding.userId.text.toString()
+        val password = binding.password.text.toString()
+        val passwordConfirmation = binding.passwordConfirmation.text.toString()
+        val email = binding.email.text.toString()
+        val nickname = binding.nickname.text.toString()
+
+        if(!InputValidUtil.isValidUserId(userId)) {
+            result *= 0
+            binding.userId.error = resources.getText(R.string.userIdErrorMessage)
+        }
+        if(!InputValidUtil.isValidPassword(password)) {
+            result *= 0
+            binding.password.error = resources.getText(R.string.passwordErrorMessage)
+        }
+        if(password != passwordConfirmation) {
+            result *= 0
+            binding.passwordConfirmation.error = resources.getText(R.string.passwordConfirmErrorMessage)
+        }
+        if(!InputValidUtil.isValidEmail(email)) {
+            result *= 0
+            binding.email.error = resources.getText(R.string.emailErrorMessage)
+        }
+        if(!InputValidUtil.isValidNickname(nickname)) {
+            result *= 0
+            binding.nickname.error = resources.getText(R.string.nicknameErrorMessage)
+        }
+
+        return when(result) {
+            1 -> true
+            else -> false
+        }
+    }
+}
