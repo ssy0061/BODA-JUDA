564127a9835f0036d62c03ebbd0ce6fae715437e
sangyong <dbr00061@naver.com>
2022-03-29 15:49:24 +0900

    S06P21S004-413 [mo] feat: 나머지 파일 추가

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/ProfileFragment.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/ProfileFragment.kt
@@ -1,24 +1,86 @@
 package com.aeye.nextlabel.feature.main
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import com.aeye.nextlabel.R
 import com.aeye.nextlabel.databinding.FragmentProfileBinding
 import com.aeye.nextlabel.feature.common.BaseFragment
 import com.aeye.nextlabel.feature.user.UserViewModel
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
 
 class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::bind, R.layout.fragment_profile) {
 
     private val userViewModel: UserViewModel by activityViewModels()
+    private lateinit var pieChart: PieChart
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initView()
+
+        pieChart = binding.profilePiechart
+        setupPieChart()
+        loadPieChartData()
     }
 
     private fun initView() {
 
+    }
+
+    private fun setupPieChart() {
+        pieChart.setDrawHoleEnabled(true)
+        pieChart.setUsePercentValues(true)
+        pieChart.setEntryLabelTextSize(12f)
+        pieChart.setEntryLabelColor(Color.BLACK)
+        pieChart.setCenterText("승인율")
+        pieChart.setCenterTextSize(24f)
+        pieChart.getDescription().setEnabled(false)
+
+        val legend = pieChart.getLegend()
+        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP)
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT)
+        legend.setOrientation(Legend.LegendOrientation.VERTICAL)
+        legend.setDrawInside(false)
+        legend.setEnabled(true)
+
+    }
+
+    private fun loadPieChartData() {
+        // 데이터 추가
+        val entries = mutableListOf<PieEntry>()
+        entries.add(PieEntry(0.6f, "Approved"))
+        entries.add(PieEntry(0.4f, "Denied"))
+
+        // 색 템플릿 추가
+        val colors = mutableListOf<Int>()
+        for (color in ColorTemplate.MATERIAL_COLORS) {
+            colors.add(color)
+        }
+        for (color in ColorTemplate.VORDIPLOM_COLORS) {
+            colors.add(color)
+        }
+
+        val dataSet = PieDataSet(entries, "Approval Rating")
+        dataSet.setColors(colors)
+
+        val data = PieData(dataSet)
+        data.setDrawValues(true)
+        data.setValueFormatter(PercentFormatter(pieChart))
+        data.setValueTextSize(12f)
+        data.setValueTextColor(Color.BLACK)
+
+        pieChart.setData(data)
+        pieChart.invalidate()
+
+        // 애니메이션 추가가
+       pieChart.animateY(1400, Easing.EaseInOutQuad)
     }
 }
