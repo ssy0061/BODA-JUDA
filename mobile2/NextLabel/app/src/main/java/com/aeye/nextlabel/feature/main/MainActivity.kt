118b38045e6e3a8a8ef5ffc44093431e3df36239
김민수 <ms001118@naver.com>
2022-03-30 10:46:19 +0900

    S06P21S004-415 [mo] feat: BottomNav 추가

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/MainActivity.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/MainActivity.kt
@@ -2,17 +2,17 @@ package com.aeye.nextlabel.feature.main
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import com.aeye.nextlabel.R
 import com.aeye.nextlabel.databinding.ActivityMainBinding
 import com.aeye.nextlabel.feature.common.BaseActivity
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
-    private lateinit var viewPager: ViewPager2
-    private lateinit var viewPagerAdapter: FragmentStateAdapter
-    private lateinit var tabLayout: TabLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,27 +20,11 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
     }
 
     private fun init() {
-        viewPager = binding.viewPagerMain
-        viewPagerAdapter = MainViewPagerAdapter()
-        viewPager.adapter = viewPagerAdapter
-
-        tabLayout = binding.tabLayoutMain
-        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            val tabLayoutText = arrayOf("프로젝트 설명", "기여하기")
-            tab.text = tabLayoutText[position]
-        }.attach()
-    }
-
-    inner class MainViewPagerAdapter : FragmentStateAdapter(this) {
-
-        override fun getItemCount(): Int = 2
-
-        // TODO: 뷰페이저 위치에 맞는 프래그먼트 세팅
-        override fun createFragment(position: Int): Fragment {
-            return when(position) {
-                0 -> HomeFragment()
-                else -> ContainerFragment()
-            }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView_main)
+        val navController = navHostFragment?.findNavController()
+        navController?.let {
+            binding.bottomNavMain.setupWithNavController(it)
         }
     }
+
 }
