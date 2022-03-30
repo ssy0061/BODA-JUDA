d3498212734a9fc5e18432c6a4663ecfef6abbf2
김민수 <ms001118@naver.com>
2022-03-31 00:41:02 +0900

    S06P21S004-421 [mo] feat: 리더보드 요청에 따른 결과 분기 추가

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/community/LeaderBoardFragment.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/community/LeaderBoardFragment.kt
@@ -1,23 +1,87 @@
 package com.aeye.nextlabel.feature.main.community
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.aeye.nextlabel.R
+import com.aeye.nextlabel.databinding.FragmentLeaderBoardBinding
+import com.aeye.nextlabel.feature.common.BaseFragment
+import com.aeye.nextlabel.feature.main.CommunityViewModel
+import com.aeye.nextlabel.model.dto.RankUser
+import com.aeye.nextlabel.util.Status
 
-class LeaderBoardFragment : Fragment() {
+class LeaderBoardFragment : BaseFragment<FragmentLeaderBoardBinding>(FragmentLeaderBoardBinding::bind, R.layout.fragment_leader_board) {
+    private val TAG = "LeaderBoardFragment_debuk"
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private val communityViewModel: CommunityViewModel by activityViewModels()
+    private lateinit var leaderAdapter: LeaderBoardAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+        initLiveDataObserver()
+        requestLeaderBoard()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_leader_board, container, false)
+    private fun init() {
+        leaderAdapter = LeaderBoardAdapter()
+        binding.recyclerViewLeaderF.apply {
+            adapter = leaderAdapter
+            layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+        }
+    }
+
+    private fun initLiveDataObserver() {
+        communityViewModel.leaderBoardResponseLiveData.observe(requireActivity()) {
+            if(communityViewModel.isFirstLoaded) {
+                // 두번째 로딩부터
+                when(it.status) {
+                    Status.SUCCESS -> {
+                        // TODO: adapter의 로딩 제거
+                    }
+                    Status.ERROR -> {
+                        // TODO: adapter의 로딩 제거
+                    }
+                    Status.LOADING -> {
+                        // TODO: adapter의 로딩 추가
+                    }
+                }
+            } else {
+                // 첫번째 로딩
+                when(it.status) {
+                    Status.SUCCESS -> {
+                        // TODO: 전체 플레이스 홀더 제거
+                    }
+                    Status.ERROR -> {
+                        // TODO: 전체 플레이스 홀더 제거
+                    }
+                    Status.LOADING -> {
+                        // TODO: 전체 플레이스 홀더 추가
+                    }
+                }
+            }
+            
+        }
+
+        communityViewModel.leaderBoardItems.observe(requireActivity()) {
+            updateDiff(it)
+        }
+    }
+
+    /** 첫 Fragment 생성 시 request*/
+    private fun requestLeaderBoard() {
+        if(!communityViewModel.isFirstLoaded) {
+            communityViewModel.getLeaderBoard()
+        }
+    }
+
+    private fun updateDiff(list: List<RankUser>) {
+        leaderAdapter.addUsers(list.subList(leaderAdapter.itemCount, list.lastIndex + 1))
     }
 }
