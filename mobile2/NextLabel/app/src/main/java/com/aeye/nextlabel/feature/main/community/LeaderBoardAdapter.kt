6d2a77242c55aebe9841ab81f2cf2353a49d4128
김민수 <ms001118@naver.com>
2022-03-31 01:31:50 +0900

    S06P21S004-421 [mo] fix: viewType에 따른 분기 처리

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/community/LeaderBoardAdapter.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/community/LeaderBoardAdapter.kt
@@ -1,16 +1,19 @@
 package com.aeye.nextlabel.feature.main.community
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.aeye.nextlabel.R
 import com.aeye.nextlabel.databinding.LeaderboardItemBinding
+import com.aeye.nextlabel.databinding.LeaderboardItemLoadingBinding
 import com.aeye.nextlabel.model.dto.RankUser
 
-class LeaderBoardAdapter: RecyclerView.Adapter<LeaderBoardAdapter.ViewHolder>() {
+class LeaderBoardAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private val items: MutableList<RankUser> = mutableListOf()
     private lateinit var binding: LeaderboardItemBinding
+    private val TYPE_LOADING = -1
 
     inner class ViewHolder(val binding: LeaderboardItemBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(user: RankUser) {
@@ -18,13 +21,24 @@ class LeaderBoardAdapter: RecyclerView.Adapter<LeaderBoardAdapter.ViewHolder>() 
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        binding = DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.leaderboard_item, parent, false)
-        return ViewHolder(binding)
+    inner class LoadingViewHolder(binding: LeaderboardItemLoadingBinding): RecyclerView.ViewHolder(binding.root) {
+
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(items[position])
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when(viewType) {
+            TYPE_LOADING -> LoadingViewHolder(LeaderboardItemLoadingBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+            else -> {
+                binding = DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.leaderboard_item, parent, false)
+                return ViewHolder(binding)
+            }
+        }
+
+
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if(holder is ViewHolder) holder.bind(items[position])
     }
 
     override fun getItemCount(): Int = items.size
@@ -33,5 +47,23 @@ class LeaderBoardAdapter: RecyclerView.Adapter<LeaderBoardAdapter.ViewHolder>() 
         val lastIndex = items.lastIndex
         items.addAll(list)
         notifyItemInserted(lastIndex)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if(items[position].id == -1) return TYPE_LOADING
+        return super.getItemViewType(position)
+    }
+
+    fun showLoading() {
+        items.add(RankUser(id = -1, 0, "", 0))
+        notifyItemInserted(items.lastIndex)
+    }
+
+    fun dismissLoading() {
+        val lastIndex = items.lastIndex
+        if(lastIndex > -1) {
+            items.removeAt(lastIndex)
+            notifyItemRemoved(lastIndex)
+        }
     }
 }
