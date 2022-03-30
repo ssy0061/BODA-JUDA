a9c3898a25f80f907b06ebbedfc576eb6745e434
김민수 <ms001118@naver.com>
2022-03-30 23:05:18 +0900

    S06P21S004-421 [mo] feat: LeaderBoardAdapter Data binding 적용, 생성

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/community/LeaderBoardAdapter.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/community/LeaderBoardAdapter.kt
@@ -1,4 +1,37 @@
 package com.aeye.nextlabel.feature.main.community
 
-class LeaderBoardAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.aeye.nextlabel.R
+import com.aeye.nextlabel.databinding.LeaderboardItemBinding
+import com.aeye.nextlabel.model.dto.RankUser
+
+class LeaderBoardAdapter: RecyclerView.Adapter<LeaderBoardAdapter.ViewHolder>() {
+    private val items: MutableList<RankUser> = mutableListOf()
+    private lateinit var binding: LeaderboardItemBinding
+
+    inner class ViewHolder(val binding: LeaderboardItemBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(user: RankUser) {
+            binding.user = user
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        binding = DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.leaderboard_item, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    fun addUsers(list: List<RankUser>) {
+        val lastIndex = items.lastIndex
+        items.addAll(list)
+        notifyItemInserted(lastIndex)
+    }
 }
