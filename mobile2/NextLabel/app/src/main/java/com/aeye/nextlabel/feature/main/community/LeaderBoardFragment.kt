e1978412f39c0b4abd28a85461c1929d04dd2190
김민수 <ms001118@naver.com>
2022-04-01 01:50:37 +0900

    S06P21S004-426 [mo] fix: import 최적화"

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/community/LeaderBoardFragment.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/main/community/LeaderBoardFragment.kt
@@ -2,10 +2,7 @@ package com.aeye.nextlabel.feature.main.community
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
