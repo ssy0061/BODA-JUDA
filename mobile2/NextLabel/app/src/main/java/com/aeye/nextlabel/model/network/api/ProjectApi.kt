63d5b31a01a8b2128d055f7203420ce7b4ea7c65
김민수 <ms001118@naver.com>
2022-04-01 00:11:59 +0900

    S06P21S004-425 [mo] feat: query 추가

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/model/network/api/ProjectApi.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/model/network/api/ProjectApi.kt
@@ -3,8 +3,9 @@ package com.aeye.nextlabel.model.network.api
 import com.aeye.nextlabel.model.dto.Project
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ProjectApi {
     @GET("/admin/project/list")
-    suspend fun getProjectByPage(page: Int): Response<List<Project>>
+    suspend fun getProjectByPage(@Query("page") page: Int): Response<List<Project>>
 }
