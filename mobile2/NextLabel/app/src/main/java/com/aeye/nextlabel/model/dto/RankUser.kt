22f13edac4ea0f79f77c4cd345056237ed2c1660
김민수 <ms001118@naver.com>
2022-04-01 01:53:44 +0900

    S06P21S004-426 [mo] fix: user profileImage 추가"

---
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/model/dto/RankUser.kt
@@ -0,0 +1,9 @@
+package com.aeye.nextlabel.model.dto
+
+data class RankUser(
+    val id: Int,
+    val imageAccepted: Int,
+    val nickName: String,
+    val profileImage: String,
+    val rank: Int
+)
