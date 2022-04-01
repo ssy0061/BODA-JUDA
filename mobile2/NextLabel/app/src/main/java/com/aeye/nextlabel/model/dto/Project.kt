f1368472b0cee26a139aa5556277bd3cb4c2d36b
김민수 <ms001118@naver.com>
2022-04-02 01:13:51 +0900

    S06P21S004-438 [mo] feat: Parcelize로 Parcel 구현

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/model/dto/Project.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/model/dto/Project.kt
@@ -1,5 +1,9 @@
 package com.aeye.nextlabel.model.dto
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Project(
     val accepted: Int,
     val description: String,
@@ -7,4 +11,4 @@ data class Project(
     val id: Int,
     val provider: String,
     val title: String
-)
+) : Parcelable
