ce3c666a0115591595846c0a80b21b01f504016b
김민수 <ms001118@naver.com>
2022-04-02 01:44:27 +0900

    S06P21S004-443 [mo] 라벨 업로드 projectId 매개변수 추가

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/labeling/LabelingViewModel.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/labeling/LabelingViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.aeye.nextlabel.model.dto.Label
+import com.aeye.nextlabel.model.dto.Project
 import com.aeye.nextlabel.model.network.response.LabelingResponse
 import com.aeye.nextlabel.repository.LabelingRepository
 import com.aeye.nextlabel.util.MultiPartUtil
@@ -20,18 +21,19 @@ import okhttp3.RequestBody.Companion.toRequestBody
 
 class LabelingViewModel: ViewModel() {
     var imageUrl: Uri? = null
+    var project: Project? = null
     private val labelingRepository = LabelingRepository()
 
     private val _uploadLabelResponse = MutableLiveData<Resource<LabelingResponse>>()
     val uploadLabelResponse: LiveData<Resource<LabelingResponse>>
         get() = _uploadLabelResponse
 
-    fun uploadLabel(label: Label, absolutePath: String) = viewModelScope.launch {
+    fun uploadLabel(label: Label, absolutePath: String, projectId: Int) = viewModelScope.launch {
         _uploadLabelResponse.postValue(Resource.loading(null))
         val labelRequestBody = Gson().toJson(label).toRequestBody("text/plain".toMediaType())
         val imageMultipart = MultiPartUtil.makeMultiPartBodyFile("file", absolutePath, "image/*")
         withContext(Dispatchers.IO) {
-            _uploadLabelResponse.postValue(labelingRepository.uploadLabels(labelRequestBody, imageMultipart))
+            _uploadLabelResponse.postValue(labelingRepository.uploadLabels(labelRequestBody, imageMultipart, projectId))
         }
     }
 }
