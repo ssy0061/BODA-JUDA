d6e5e8b9caa7451fdc008244dcf3bfb5f4585ac3
김민수 <ms001118@naver.com>
2022-04-02 01:44:59 +0900

    S06P21S004-443 [mo] feat: Intent extra로 Project 받기

--- mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/camera/CameraActivity.kt
+++ mobile2/NextLabel/app/src/main/java/com/aeye/nextlabel/feature/camera/CameraActivity.kt
@@ -12,14 +12,23 @@ import com.aeye.nextlabel.feature.labeling.LabelingViewModel
 import com.aeye.nextlabel.feature.user.JoinFragment
 import com.aeye.nextlabel.feature.user.LoginFragment
 import com.aeye.nextlabel.global.*
+import com.aeye.nextlabel.model.dto.Project
 
 class CameraActivity : BaseActivity<ActivityCameraBinding>(ActivityCameraBinding::inflate) {
     private val  labelingViewModel: LabelingViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getIntentExtra()
         init()
     }
+
+    private fun getIntentExtra() {
+        intent.getParcelableExtra<Project>(PROJECT_EXTRA)?.let {
+            labelingViewModel.project = it
+        }
+    }
+
     fun init() {
         setFragment()
         supportFragmentManager.setFragmentResultListener(MOVE_FRAGMENT, this) { _, bundle ->
